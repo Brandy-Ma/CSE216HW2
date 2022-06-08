655503d74a2afc31bc26400bbf2d67afff278a1b
Brandy-Ma <106458292+Brandy-Ma@users.noreply.github.com>
2022-06-08 13:46:50 -0400

    Get Function Fixxed

--- Part A/js/OpenAddressHashTable.js
+++ Part A/js/OpenAddressHashTable.js
@@ -5,7 +5,6 @@ class KeyValuePair {//hjerherh
     }
     
     toString() {
-        console.log("TESTER");
         return "(" + this.key + ", " + this.value.toString() + ")";
     }
 }
@@ -48,10 +47,11 @@ export default class OpenAddressHashTable {
     
     // @todo - YOU MUST DEFINE THIS METHOD
     getValue(key) {
+        let temKey = key;
         let index = this.hashCode(key);
-        if(this.hashTable[index] === null)
+        if(this.hashTable[index] != null)
         {
-            if(this.hashTable[index].key === key)
+            if(this.hashTable[index].key === temKey)
             {
                 return this.hashTable[index];
             }
@@ -59,21 +59,21 @@ export default class OpenAddressHashTable {
         //wrap if it isn't there
         for(let i = index; i < this.hashTable.length; i++)
         {
-            if(this.hashTable[i] !== null)
+            if(this.hashTable[i] != null)
             {
-                if(this.hashTable[index].key === key )
+                if(this.hashTable[i].key === temKey )
                 {
-                    return this.hashTable[index];
+                    return this.hashTable[i];
                 }
             }
         }
         for(let i = 0; i < index; i++)
         {
-            if(this.hashTable[i] !== null)
+            if(this.hashTable[i] != null)
             {
-                if(this.hashTable[index].key === key)
+                if(this.hashTable[i].key === temKey)
                 {
-                    return this.hashTable[index];
+                    return this.hashTable[i];
                 }
             }
         }
@@ -88,7 +88,104 @@ export default class OpenAddressHashTable {
     putValue(key, item) {
         let newKeyValuePair = new KeyValuePair(key, item);
         let index = this.hashCode(key);
+        if(this.getValue(newKeyValuePair.key) != null)
+        {
+            for(let i = 0; i<this.hashTable.length; i++)
+            {
+                if(this.hashTable[i] != null)
+                {
+                    if(this.hashTable[i].key === key)
+                    {
+                        this.hashTable[i] = newKeyValuePair;
+                    }
+                }
+            }
+        }
+        //if value doesn't exist you start from hashindex until future
+        // from index to length
+        if(this.hashTable[index] == null)
+        {
+            this.hashTable[index] = newKeyValuePair;
+            this.size = this.size + 1;
+            return;
+        }
+        // index -> end
+        for(let i = index; i<this.hashTable.length; i++)
+        {
+            if(this.hashTable[i]==null)
+            {
+                this.hashTable[i] == newKeyValuePair;
+                this.size = this.size + 1;
+                return;
+            }
+        }
+        //beginning ->index
+        for(let i = 0; i<index; i++)
+        {
+            if(this.hashTable[i]==null)
+            {
+                this.hashTable[i] == newKeyValuePair;
+                this.size = this.size + 1;
+                return;
+            }
+        }
+        if(this.size === this.length)
+        {
+            rehashTable();
+            this.putValue(key,item);
+        }
         this.hashTable[index] = newKeyValuePair;
+    }
+
+    rehashTable()
+    {
+        let oldLength = this.length;
+        this.length = this.length * 2;
+        let hashTableNew = [];
+        for(let i=0; i<oldLength; i++)
+        {
+            if(this.hashTable[i] != null)
+            {
+                let nullSpotFound = true;
+                let newHashCode = this.hashCode(this.hashTable[i].key);
+                if(hashTableNew[newHashCode]==null)
+                {
+                    hashTableNew = this.hashTable[i];
+                    nullSpotFound = false;
+                }
+                //if spot isn't empty linearly probe
+                if(nullSpotFound)
+                {
+                    for(let j = newHashCode; j<this.length; j++)
+                    {
+                        if(nullSpotFound)
+                        {
+                            if(hashTableNew[j] == null)
+                            {
+                                hashTableNew[j] = this.hashTable[i];
+                                nullSpotFound = false;
+                            }
+                        }
+                    }
+                }
+                //if still not found restart from beginning to new hash code
+                if(nullSpotFound)
+                {
+                    for(let j = 0; j < newHashCode; j++)
+                    {
+                        if(nullSpotFound)
+                        {
+                            if(hashTableNew[j] == null)
+                            {
+                                hashTableNew[j] = this.hashTable[i];
+                                nullSpotFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        this.hashTable = hashTableNew;
     }
     
     toString() {
--- Part A/js/Test.js
+++ Part A/js/Test.js
@@ -21,14 +21,14 @@ function addPersonToHashTable(person, hashTable) {
 
 let hashTable = new OpenAddressHashTable(NUM_BINS, KEY_LENGTH);
 
-// // DEMONSTRATE ADDING VALUES TO THE HASH TABLE, WHICH INCLUDES THE NEED TO MAKE THE HASH TABLE BIGGER
-// addPersonToHashTable(new Student(hashTable.generateKey(), "George", "Harrison", 4.0), hashTable);
-// addPersonToHashTable(new Employee(hashTable.generateKey(), "Paul", "McCartney", 80000), hashTable);
-// addPersonToHashTable(new Employee(hashTable.generateKey(), "Ringo", "Starr", 40000), hashTable);
-// addPersonToHashTable(new Person(hashTable.generateKey(), "Chuck", "Berry"), hashTable);
-// addPersonToHashTable(new Student(hashTable.generateKey(), "Mick", "Jagger", 3.5), hashTable);
-// addPersonToHashTable(new Student(hashTable.generateKey(), "Jimi", "Hendrix", 3.6), hashTable);
-// addPersonToHashTable(new Person(hashTable.generateKey(), "Roger", "Waters"), hashTable);
+// DEMONSTRATE ADDING VALUES TO THE HASH TABLE, WHICH INCLUDES THE NEED TO MAKE THE HASH TABLE BIGGER
+addPersonToHashTable(new Student(hashTable.generateKey(), "George", "Harrison", 4.0), hashTable);
+addPersonToHashTable(new Employee(hashTable.generateKey(), "Paul", "McCartney", 80000), hashTable);
+addPersonToHashTable(new Employee(hashTable.generateKey(), "Ringo", "Starr", 40000), hashTable);
+addPersonToHashTable(new Person(hashTable.generateKey(), "Chuck", "Berry"), hashTable);
+addPersonToHashTable(new Student(hashTable.generateKey(), "Mick", "Jagger", 3.5), hashTable);
+addPersonToHashTable(new Student(hashTable.generateKey(), "Jimi", "Hendrix", 3.6), hashTable);
+addPersonToHashTable(new Person(hashTable.generateKey(), "Roger", "Waters"), hashTable);
 
 // DEMONSTRATE MAKING KEYS AND ADDING VALUES TO THE HASH TABLE    
 let jlKey = hashTable.generateKey();
@@ -47,11 +47,11 @@ console.log("\nget " + cwKey + ": " + p.toString() + "\n");
 p = hashTable.getValue(dgKey);
 console.log("\nget " + dgKey + ": " + p.toString() + "\n");
 
-// NOW LET'S TRY REPLACING THE DATA IN THE ABOVE THREE
-// hashTable.putValue(jlKey, new Student(jlKey, "Otis", "Redding", 3.5));
-// hashTable.putValue(cwKey, new Student(cwKey, "Keith", "Richards", 3.1));
-// hashTable.putValue(dgKey, new Student(dgKey, "Bill", "Withers", 3.4));
-// printHashTable("\nAfter Changing 3 Items", hashTable);
+//NOW LET'S TRY REPLACING THE DATA IN THE ABOVE THREE
+hashTable.putValue(jlKey, new Student(jlKey, "Otis", "Redding", 3.5));
+hashTable.putValue(cwKey, new Student(cwKey, "Keith", "Richards", 3.1));
+hashTable.putValue(dgKey, new Student(dgKey, "Bill", "Withers", 3.4));
+printHashTable("\nAfter Changing 3 Items", hashTable);
 
 // // AND DEMONSTRATE REMOVING ITEMS FROM THE BST
 // hashTable.removeValue(jlKey);
